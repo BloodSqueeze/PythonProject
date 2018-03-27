6ebc957945b6603d823a2d72eb46317bfb852255
Erick Ledesma <erickledesma@gmail.com>
2018-03-27 11:24:43 -0500

    deleted extra html code

--- MusicFy/MusicFy/settings.py
+++ MusicFy/MusicFy/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debut_toolbar',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'MusicFy.urls'
