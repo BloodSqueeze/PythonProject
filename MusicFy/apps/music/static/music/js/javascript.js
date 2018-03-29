444db7e682fb7a99dd457a76cbb91c8ffcd6e059
Erick Ledesma <erickledesma@gmail.com>
2018-03-29 15:03:58 -0500

    nothing new

--- MusicFy/apps/music/static/music/js/javascript.js
+++ MusicFy/apps/music/static/music/js/javascript.js
@@ -26,17 +26,7 @@ $(document).ready(function(){
 
 /* Javascript for Napster API */
 
-const tracksTemplateSource = document.getElementById('tracks-template').innerHTML;
-const tracksTemplate = Handlebars.compile(tracksTemplateSource);
 
-const $tracks = $('#tracks-container');
 
-const getTopTracks = $.get('https://api.napster.com/v2.1/tracks/top?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm');
-
-getTopTracks
-  .then((response) => {
-    $tracks.html(tracksTemplate(response));
-  });
-      
 
             
